c1a13f53cda0b47883fe7c7308a949199c319949
Kiran Shenoy P.H <33525945+KiranShny@users.noreply.github.com>
2017-11-23 14:21:15 +0530

    Create 11search.php

---
+++ 11/11search.php
@@ -0,0 +1,16 @@
+<! DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>DATABASE PHP</title>
+</head>
+<body bgcolor=black>
+<div style="background-color:red;"><b><p align="center">DIGITAL ADDRESS BOOK</p></div>
+<p><a href="lab11a.php"><img src="index.jpg" alt=" " width="70" height="70"></a></p>
+<h1 align="center" >Search Details</h1>
+<form name="a2" action="display.php" method="POST">
+<input style="margin-left:500px;" type="text" name="search" id="id1" placeholder="ENTER NAME" onsubmit="javascript:submit();"/>
+</form>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<div style="background-color:grey;"><b><p align="center">CopyRight @</p></div>
+</body>
+</html
