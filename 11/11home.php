43b46e15568249342ce46e75a42034d751276762
Kiran Shenoy P.H <33525945+KiranShny@users.noreply.github.com>
2017-11-23 14:18:58 +0530

    Create 11home.php

---
+++ 11/11home.php
@@ -0,0 +1,19 @@
+<! DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>DATABASE PHP</title>
+</head>
+<body bgcolor=black>
+<div style="background-color:red;"><b><p align="center">DIGITAL ADDRESS BOOK</p></div>
+<form action="11add.php">
+<input style="margin-left:100px;font-size:25px" type="submit" value="ADD DETAILS">
+</form>
+<form action="11search.php">
+<input style="margin-left:100px;font-size:25px" type="submit" value="SEARCH DETAILS">
+</form>
+
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<div style="background-color:grey;"><b><p align="center">CopyRight @</p></div>
+</form>
+</body>
+</html>
